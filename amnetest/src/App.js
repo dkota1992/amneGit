9d485c1c056103335f0eda8672b9da7785a46f6f
Deepak Kota <dkota1992@gmail.com>
2017-08-28 00:19:53 -0400

    After Careful

--- amnetest/src/App.js
+++ amnetest/src/App.js
@@ -4,26 +4,20 @@ import NavBar from "./Components/Mainbar.js";
 import InputField from "./Components/input.js"
 import './App.css'
 
-const buttonStyle = {
-  margin: "20px 20px 20px 20px",
-  backgroundColor: "orange"
-};
+
 class App extends Component {
-
-  handleChange (){
-
+  constructor(props){
+    super(props);
+    this.state = {};
   }
+
   render() {
     return (
       <div>
         <NavBar />
       <div className="inputField">
-          <h4>First Address: </h4>
-          <InputField />
-          <h4>Second Address: </h4>
-          <InputField />
-          <button style={buttonStyle}>Fetch Agencies </button>
-        </div>
+            <InputField />
+      </div>
       </div>
     );
   }
--- amnetest/src/Components/input.js
+++ amnetest/src/Components/input.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import Autocomplete from 'react-google-autocomplete';
 
+const buttonStyle = {
+  margin: "20px 20px 20px 20px",
+  backgroundColor: "orange"
+};
+
 class InputField extends React.Component{
   constructor(props){
     super(props);
-    this.state = {textValue : ""};
+    this.state = {firstAddress: "", secondAddress:""};
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handlechange(e){
-    this.setState({textValue: e.target.value});
+  handleChange(e){
+    var text = e.target.value;
+    var oldState = this.state;
+    if (e.target.getAttribute("id") === "firstAddress"){
+      this.setState({firstAddress : text, secondAddress: oldState.secondAddress});
+    }
+    else{
+      this.setState({firstAddress: oldState.firstAddress, secondAddress: text});
+    }
     e.preventDefault();
   }
 
   render(){
     return(
       <div>
-        <input onChange={this.handlechange.bind(this)} type="text" />
-        <h3>{this.state.textValue} </h3>
+        <h4>First Address </h4>
+        <input onChange={this.handleChange} type="text" id="firstAddress" value={this.props.value} />
+        <h5>{this.state.firstAddress} </h5>
+        <h4>Second Address </h4>
+        <input onChange={this.handleChange} type="text" id="secondAddress" value={this.props.value} />
+        <h5>{this.state.secondAddress} </h5>
+        <button style={buttonStyle} onClick={this.handleSubmit}>Fetch Agencies </button>
       </div>
     );
   }
