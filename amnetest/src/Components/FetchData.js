2cd870535b533f8f9acfb3327c15fb3956338afc
Deepak Kota <dkota1992@gmail.com>
2017-08-28 15:47:53 -0400

    After Willmount

--- amnetest/src/Components/FetchData.js
+++ amnetest/src/Components/FetchData.js
@@ -1,12 +1,47 @@
 import React from 'react';
-
+import {GoogleMapLoader, GoogleMap, Marker} from 'react-google-maps'
+import axios from 'axios';
+var key = 'AIzaSyAPiTf3PgAhAI1iFblJrTlt8GXbHOgJXQ0'
 class FetchData extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {firstAddress : "", secondAddress: "", firstAddressFetch: "", secondAddressFetch: ""};
+  }
+
+  componentDidMount(){
+    console.log("Wassup Assole");
+  }
+
+  componentWillMount(){
+    var location = this.props.state.firstAddress;
+    var self = this;
+    axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+      params: {
+        address: location,
+        key: key
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+      console.log(response.data);
+       self.setState({firstAddress: response.data});
+     })
+     .catch(function(error){
+       console.log(error);
+     });
+}
+
   render(){
     return(
-      <h1>{this.props.address.firstAddress}</h1>
+      <div style={{width:300, height:300, backgroundColor:"green"}}>
+          {this.state.firstAddress.status}
+      </div>
     );
   }
 }
+
+
+
 
 
 export default FetchData;
--- amnetest/src/Components/input.js
+++ amnetest/src/Components/input.js
@@ -14,35 +14,20 @@ class InputField extends React.Component{
   constructor(props){
     super(props);
     this.state = {firstAddress: "", secondAddress:"", radius: "10", searchterm: "Real estate Agencies", result : ""};
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleChange(e){
-    var text = e.target.value;
-    var state = this.state;
-    if (e.target.getAttribute("id") === "firstAddress"){
-      state.firstAddress = text;
-    }
-    else{
-      state.secondAddress = text;
-    }
-    this.setState(state);
 
-    e.preventDefault();
-  }
 
 handleInputChange(e){
   var state = this.state;
   var text = e.target.value;
-  if(e.target.getAttribute("id") === "radius"){
-    state.radius = text;
-  } else{
-    state.searchterm = text;
-  }
+  var id = e.target.getAttribute("id");
+  state[id] = text;
   this.setState(state);
+  e.preventDefault();
 }
 
 
@@ -51,9 +36,9 @@ handleSubmit(e){
   if ((state.firstAddress === "") || (state.secondAddress === "")){
     alert("Both fields are required.")
   }
-  state.result = <FetchData address={this.state} />;
-
-  this.setState(state)
+  state['result'] = <FetchData state={this.state}/>
+  console.log(this.state);
+  this.setState(state);
 }
 
   handleSelect(place,id){
@@ -72,17 +57,17 @@ handleSubmit(e){
     return(
       <div>
         <h4>First Address: </h4>
-        <Autocomplete onChange={this.handleChange} onClick={this.handleChange} id="firstAddress" style={{width: '190%'}}
+        <Autocomplete onChange={this.handleInputChange} onClick={this.handleInputChange} id="firstAddress" style={{width: '190%'}}
         onPlaceSelected={(place) => this.handleSelect(place,"firstAddress")} types={['address']} />
         <h5 style={{color: "blue"}}>{this.state.firstAddress} </h5>
         <h4>Second Address: </h4>
-        <Autocomplete onChange={this.handleChange} onClick={this.handleChange} id="secondAddress" style={{width: '190%'}}
+        <Autocomplete onChange={this.handleInputChange} onClick={this.handleInputChange} id="secondAddress" style={{width: '190%'}}
         onPlaceSelected={(place) => this.handleSelect(place,"secondAddress")} types={['address']} />
         <h5 style={{color:"blue"}}>{this.state.secondAddress} </h5>
         <h4>Enter Radius(Miles):</h4>
-        <input type="text" id="radius" onChange={this.handleInputChange}/>
+        <input type="text" id="radius" placeholder="10" onChange={this.handleInputChange}/>
         <h4>Enter Search Term: </h4>
-        <input type="text" id="searchterm" value="Real Estate Agencies" onChange={this.handleInputChange}/>
+        <input type="text" id="searchterm" placeholder="Real Estate Agencies" onChange={this.handleInputChange}/>
 
         <button style={buttonStyle} onClick={this.handleSubmit}>Fetch Agencies </button>
 
